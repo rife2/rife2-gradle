0e16f1d741238f4255cc06b273227d7a6e38747d
Geert Bevin <gbevin@uwyn.com>
2023-03-05 12:23:51 -0500

    Initial version

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    `maven-publish`
+    `java-gradle-plugin`
+    groovy
+    id("com.gradle.plugin-publish") version "1.1.0"
+}
+
+group = "com.uwyn.rife2"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    gradleApi()
+    testImplementation(libs.spock.core)
+    testImplementation(gradleTestKit())
+}
+
+gradlePlugin {
+    website.set("https://rife2.com")
+    vcsUrl.set("https://github.com/gbevin/rife2-gradle")
+
+    plugins {
+        create("rife2") {
+            id = "com.uwyn.rife2"
+            displayName = "RIFE2 Plugin"
+            description = "Provides support for easily building RIFE2 applications with Gradle"
+            tags.set(listOf("plugins", "rife2", "web-framework", "java"))
+            implementationClass = "com.uwyn.rife2.gradle.Rife2Plugin"
+        }
+    }
+}
+
+tasks {
+    javadoc {
+        options {
+            this as StandardJavadocDocletOptions
+            keyWords(true)
+            splitIndex(true)
+            addBooleanOption("Xdoclint:-missing", true)
+        }
+    }
+
+    withType<Test>().configureEach {
+        useJUnitPlatform()
+        testLogging {
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            events = setOf(org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED, org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED)
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "localPluginRepository"
+            url = uri("../local-plugin-repository")
+        }
+    }
+}
